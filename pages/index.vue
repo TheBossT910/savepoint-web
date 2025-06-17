02e81b749055bb4656a6e2a795eff9525b506059
Taha Rashid <taharashid710192@gmail.com>
2025-06-16 21:36:31 -0400

    chore: added buttons to showcase theming

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        Home page
-        <button class="btn btn-primary">Daisy-UI Button</button>
+    <div class="flex flex-wrap gap-x-4 gap-y-3 m-3">
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-secondary">Secondary</button>
+        <button class="btn btn-accent">Accent</button>
+        <button class="btn btn-neutral">Neutral</button>
+        <button class="btn btn-info">Info</button>
+        <button class="btn btn-success">Success</button>
+        <button class="btn btn-warning">Danger</button>
+        <button class="btn btn-error">Error</button>
     </div>
 </template>
