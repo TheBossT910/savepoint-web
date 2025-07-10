9b2b6ea1424ac0d635bbd5ce8c9ecd3d33490e56
Taha Rashid <taharashid710192@gmail.com>
2025-07-09 22:01:37 -0400

    feat: created retailer card

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -317,7 +317,7 @@
             <!-- Description -->
             <div class="mt-[-100vh] flex flex-col px-10 pb-5">
                 <div class=" mt-5 w-full md:w-[50%] h-auto bg-gray-200/60 dark:bg-gray-800/60 border-[1px] border-gray-100/70 backdrop-blur-[5px] rounded-[12px]">
-                    <div class="p-2 dm-sans-bold text-[18px] text-white font-outline">
+                    <div class="px-10 py-8 dm-sans-bold text-[18px] text-white font-outline">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, maiores aut. Consectetur alias assumenda totam nemo, id laboriosam necessitatibus minima nisi, dolor delectus natus accusamus voluptates. Enim labore sit delectus.
                         Placeat laborum commodi accusamus nulla nisi optio nam voluptatum eligendi in reiciendis sint ipsum pariatur, impedit exercitationem sunt vero. Repudiandae quisquam repellendus quaerat in quidem eligendi voluptas illo officiis? Tempora?
                         Expedita enim a quibusdam magnam ipsam quia, suscipit saepe dicta? Eaque omnis consequatur, ad eligendi est cupiditate atque odio blanditiis soluta temporibus natus ut quae. Libero soluta accusantium inventore omnis.
@@ -340,6 +340,81 @@
         <div class="relative px-10">
             <div class="dm-sans-bold text-[48px] text-black w-full h-auto">
                 Retailers
+            </div>
+
+            <div class="flex">
+                <div class="w-1/2">
+                    <div class="flex flex-row">
+                        <!-- Retailer card -->
+                        <div class="w-[90%] bg-gray-300 border-gray-300 border-[10px] rounded-[12px]">
+                            <div class="flex bg-gray-100 rounded-[12px]">
+                                <div class="w-[50%] px-2">
+                                    <!-- Title -->
+                                    <div class="dm-sans-bold text-[48px] text-black w-full h-auto">
+                                        EB Games
+                                    </div>
+
+                                    <!-- Stars -->
+                                    <div class="flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-black ml-1" viewBox="0 0 16 16">
+                                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-black ml-1" viewBox="0 0 16 16">
+                                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-black ml-1" viewBox="0 0 16 16">
+                                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-black ml-1" viewBox="0 0 16 16">
+                                            <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-black ml-1" viewBox="0 0 16 16">
+                                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                                        </svg>
+                                    </div>
+                                </div>
+
+                                <div class="w-[15%] p-2 my-auto">
+                                    <div class="bg-green-500/60 border-green-400 border-[1px] p-2 rounded-[12px]">
+                                        <div class="dm-sans-bold text-[16px] text-white w-full h-auto">
+                                            $9.99 - $49.99
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="w-[35%] p-2 gap-y-2">
+                                    <!-- Condiitons -->
+                                    <div class="bg-blue-600/60 border-blue-500 border-[1px] px-2 mb-2 rounded-[12px]">
+                                        <div class="dm-sans-bold text-[16px] text-white w-full h-auto">
+                                            New
+                                        </div>
+                                    </div>
+
+                                    <div class="bg-green-500/60 border-green-400 border-[1px] px-2 mb-2 rounded-[12px]">
+                                        <div class="dm-sans-bold text-[16px] text-white w-full h-auto">
+                                            Complete
+                                        </div>
+                                    </div>
+
+                                    <div class="bg-yellow-500/60 border-yellow-400 border-[1px] px-2 rounded-[12px]">
+                                        <div class="dm-sans-bold text-[16px] text-white w-full h-auto">
+                                            Used
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="w-1/2">
+                <div class="flex flex-row justify-center">
+                    <!-- Map -->
+                    <div class="bg-blue-500 w-full h-[690px]">
+                        Map
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>
