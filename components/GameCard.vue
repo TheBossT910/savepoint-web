4c2675e1626a172539eec03b62da9c59ceeb18a6
Taha Rashid <taharashid710192@gmail.com>
2025-08-02 08:55:05 -0400

    feat: initial implementation of updated ProductCard (now called GameCard)

---
+++ components/GameCard.vue
@@ -0,0 +1,106 @@
+<!-- Taha Rashid -->
+<!-- Aug 2, 2025 -->
+
+<template>
+    <!-- Outer Product Card -->
+    <div :id="`product-card-${props.id}`" class="bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] rounded-2xl p-[5px] shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out will-change-transform" @click="goToProduct">
+        <div class="relative aspect-[2/3] rounded-2xl transition-all duration-300">
+            <!-- Image -->
+            <img 
+            :src="props.coverImage"
+            class="w-full aspect-[2/3] object-cover rounded-2xl"
+            >
+
+            <!-- Buy button -->
+            <div class="flex absolute left-0 right-0 bottom-2 bg-[#9B5BE9]/70 border-[1px] border-[#7B28E0]/70 backdrop-blur-[5px] font-outline dm-sans-bold text-[12px] text-white h-[55px] w-[95%] mx-auto rounded-2xl">
+                <div class="m-auto">
+                    $ Buy
+                </div>
+            </div>
+
+            <!-- Platform badges -->
+            <div class="flex absolute left-0 right-0 bottom-18 bg-[#10A4DA]/70 border-[1px] border-[#26C1E0]/70 backdrop-blur-[5px] h-[25px] w-[95%] mx-auto rounded-2xl">
+                <div class="w-full flex flex-wrap gap-x-2 gap-y-2 m-auto">
+                    <img
+                    v-for="logo in props.platformLogos" :key="logo" 
+                    :src="logo"
+                    class="mx-auto max-h-[13px] invert"
+                    >
+            </div>
+            </div>
+
+            <!-- Owned badge -->
+            <div class="flex absolute top-[16px] left-[16px] bg-[#10A4DA]/70 border-[1px] border-[#26C1E0]/70 backdrop-blur-[5px] w-[58px] h-[22px] rounded-2xl font-outline dm-sans-bold text-white text-[12px]">
+                <div class="m-auto">
+                    Owned
+                </div>
+            </div>
+
+            <!-- Favorite badge -->
+            <div class="flex items-center justify-center text-base-900 dark-font-outline absolute top-[16px] right-[16px] bg-[#EF4444]/70 border-[1px] border-[#F87171]/70 backdrop-blur-[5px] w-[22px] h-[22px] rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                </svg>
+            </div>
+        </div>
+
+        <!-- Content -->
+        <div class="flex flex-col w-full px-1 py-1">
+            <div class="text-base-900 dark-font-outline dm-sans-bold w-full min-h-[23px] text-[18px]">
+                {{ props.title }}
+            </div>
+
+            <div class="text-gray-400 dm-sans-bold w-full min-h-[16px] text-[12px]">
+                {{ props.developers.join(', ') }}, {{ props.year }}
+            </div>
+
+            <div class="text-gray-400 dm-sans-bold w-full min-h-[16px] text-[12px] mt-1">
+                {{ props.generes.join(', ') }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { IProductCardProp } from '~/types';
+
+const props = defineProps<IProductCardProp>()
+let card: HTMLElement | null = null
+
+onMounted(() => {
+    card = document.getElementById(`product-card-${props.id}`)
+    // add event listeners
+    if (!card) return
+    card.addEventListener("mousemove", mouseMove)
+    card.addEventListener("mouseleave", mouseLeave)
+})
+
+onUnmounted(() => {
+    // remove listeners
+    if (!card) return 
+    card.removeEventListener('mousemove', mouseMove)
+    card.removeEventListener('mouseleave', mouseLeave)
+})
+
+const mouseMove = (e: MouseEvent) => {
+    if (!card) return
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left // x position within the card
+    const y = e.clientY - rect.top  // y position within the card
+
+    // Degrees to rotate by
+    const rotateX = ((y / rect.height) - 0.5) * 6
+    const rotateY = ((x / rect.width) - 0.5) * -6
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+}
+
+const mouseLeave = () => {
+    if (!card) return
+    card.style.transform = "rotateX(0deg) rotateY(0deg)"
+}
+
+const goToProduct = () => {
+    console.log('goToProduct clicked')
+}
+</script>
---
+++ pages/developer/componentDisplay.vue
@@ -0,0 +1,194 @@
+<!-- Taha Rashid -->
+<!-- Aug 2, 2025 -->
+
+<template>
+    <div>
+        <!-- Products -->
+        <div class="px-20 pb-10 pt-30">
+            <div class="w-[30%]">
+                <GameCard :="productCardProps[0]"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { IProductCardProp } from '~/types'
+
+const productCardProps: IProductCardProp[] = [
+    {
+        id: 0,
+        coverImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
+        isOwned: false,
+        isFavorite: false,
+        developers: ['Atlus'],
+        year: 2024,
+        title: 'Persona 3: Reload',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
+            'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png'
+        ],
+        generes: ['Action', 'Adventure', 'Horror']
+    },
+    {
+        id: 1,
+        coverImage: 'https://cdn2.steamgriddb.com/thumb/c2526ac91cf2dba8f8b1b58935c7597c.jpg',
+        isOwned: true,
+        isFavorite: true,
+        developers: ['Nintendo'],
+        year: 2025,
+        title: 'Mario Kart World',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Nintendo_Switch_2_logo_transparent_black.svg'
+        ],
+        generes: ['Action', 'Multiplayer', 'Racing', 'Open World', 'Family-Friendly', 'Party', '4-Player']
+    },
+     {
+        id: 2,
+        coverImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
+        isOwned: false,
+        isFavorite: false,
+        developers: ['Atlus'],
+        year: 2024,
+        title: 'Persona 3: Reload',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
+            'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png'
+        ],
+        generes: ['Action', 'Adventure', 'Horror']
+    },
+    {
+        id: 3,
+        coverImage: 'https://cdn2.steamgriddb.com/thumb/c2526ac91cf2dba8f8b1b58935c7597c.jpg',
+        isOwned: true,
+        isFavorite: true,
+        developers: ['Nintendo'],
+        year: 2025,
+        title: 'Mario Kart World',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Nintendo_Switch_2_logo_transparent_black.svg'
+        ],
+        generes: ['Action', 'Multiplayer', 'Racing', 'Open World', 'Family-Friendly', 'Party', '4-Player']
+    },
+     {
+        id: 4,
+        coverImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
+        isOwned: false,
+        isFavorite: false,
+        developers: ['Atlus'],
+        year: 2024,
+        title: 'Persona 3: Reload',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
+            'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png'
+        ],
+        generes: ['Action', 'Adventure', 'Horror']
+    },
+    {
+        id: 5,
+        coverImage: 'https://cdn2.steamgriddb.com/thumb/c2526ac91cf2dba8f8b1b58935c7597c.jpg',
+        isOwned: true,
+        isFavorite: true,
+        developers: ['Nintendo'],
+        year: 2025,
+        title: 'Mario Kart World',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Nintendo_Switch_2_logo_transparent_black.svg'
+        ],
+        generes: ['Action', 'Multiplayer', 'Racing', 'Open World', 'Family-Friendly', 'Party', '4-Player']
+    },
+     {
+        id: 6,
+        coverImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
+        isOwned: false,
+        isFavorite: false,
+        developers: ['Atlus'],
+        year: 2024,
+        title: 'Persona 3: Reload',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
+            'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png'
+        ],
+        generes: ['Action', 'Adventure', 'Horror']
+    },
+    {
+        id: 7,
+        coverImage: 'https://cdn2.steamgriddb.com/thumb/c2526ac91cf2dba8f8b1b58935c7597c.jpg',
+        isOwned: true,
+        isFavorite: true,
+        developers: ['Nintendo'],
+        year: 2025,
+        title: 'Mario Kart World',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Nintendo_Switch_2_logo_transparent_black.svg'
+        ],
+        generes: ['Action', 'Multiplayer', 'Racing', 'Open World', 'Family-Friendly', 'Party', '4-Player']
+    },
+     {
+        id: 8,
+        coverImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
+        isOwned: false,
+        isFavorite: false,
+        developers: ['Atlus'],
+        year: 2024,
+        title: 'Persona 3: Reload',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
+            'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png'
+        ],
+        generes: ['Action', 'Adventure', 'Horror']
+    },
+    {
+        id: 9,
+        coverImage: 'https://cdn2.steamgriddb.com/thumb/c2526ac91cf2dba8f8b1b58935c7597c.jpg',
+        isOwned: true,
+        isFavorite: true,
+        developers: ['Nintendo'],
+        year: 2025,
+        title: 'Mario Kart World',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Nintendo_Switch_2_logo_transparent_black.svg'
+        ],
+        generes: ['Action', 'Multiplayer', 'Racing', 'Open World', 'Family-Friendly', 'Party', '4-Player']
+    },
+     {
+        id: 10,
+        coverImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
+        isOwned: false,
+        isFavorite: false,
+        developers: ['Atlus'],
+        year: 2024,
+        title: 'Persona 3: Reload',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
+            'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png'
+        ],
+        generes: ['Action', 'Adventure', 'Horror']
+    },
+    {
+        id: 11,
+        coverImage: 'https://cdn2.steamgriddb.com/thumb/c2526ac91cf2dba8f8b1b58935c7597c.jpg',
+        isOwned: true,
+        isFavorite: true,
+        developers: ['Nintendo'],
+        year: 2025,
+        title: 'Mario Kart World',
+        platformLogos: [
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Nintendo_Switch_2_logo_transparent_black.svg'
+        ],
+        generes: ['Action', 'Multiplayer', 'Racing', 'Open World', 'Family-Friendly', 'Party', '4-Player']
+    },
+]
+</script>
