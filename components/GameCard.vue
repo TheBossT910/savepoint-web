a1b0a7ee94eaba3337a7420ca46648caa0c7df12
Taha Rashid <taharashid710192@gmail.com>
2025-08-03 16:08:21 -0400

    chore: updated ids

--- components/GameCard.vue
+++ components/GameCard.vue
@@ -2,8 +2,8 @@
 <!-- Aug 2, 2025 -->
 
 <template>
-    <!-- Outer Product Card -->
-    <div :id="`product-card-${props.id}`" class="bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] rounded-2xl p-[5px] shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out will-change-transform" @click="goToProduct">
+    <!-- Outer Card -->
+    <div :id="`game-card-${props.id}`" class="bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] rounded-2xl p-[5px] shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out will-change-transform" @click="goToProduct">
         <div class="relative aspect-[2/3] rounded-2xl transition-all duration-300">
             <!-- Image -->
             <img 
