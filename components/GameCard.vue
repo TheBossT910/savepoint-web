f856327b76c206b572779e2958b3c40dcbf2626c
Taha Rashid <taharashid710192@gmail.com>
2025-08-03 16:03:32 -0400

    style: favourite heart always white

--- components/GameCard.vue
+++ components/GameCard.vue
@@ -37,7 +37,7 @@
             </div>
 
             <!-- Favorite badge -->
-            <div class="flex items-center justify-center text-base-900 dark-font-outline absolute top-[16px] right-[16px] bg-[#EF4444]/70 border-[1px] border-[#F87171]/70 backdrop-blur-[5px] w-[22px] h-[22px] rounded-full">
+            <div class="flex items-center justify-center text-white dark-font-outline absolute top-[16px] right-[16px] bg-[#EF4444]/70 border-[1px] border-[#F87171]/70 backdrop-blur-[5px] w-[22px] h-[22px] rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
                 </svg>
