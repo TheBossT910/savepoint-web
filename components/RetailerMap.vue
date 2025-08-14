7c2f4eb92ff0ef0dc56983572bea49a7c0db4802
Taha Rashid <taharashid710192@gmail.com>
2025-08-13 22:15:57 -0400

    feat: applied new design and styling

--- components/RetailerMap.vue
+++ components/RetailerMap.vue
@@ -3,82 +3,61 @@
 
 <template>
     <div>
-        <div class="w-full">
-            <div class="flex flex-col">
+        <div class=" bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] rounded-3xl p-1 shadow-[0_0_10px_rgba(38,75,224,0.25)] transition-all duration-300 ease-out">
+            <!-- Top -->
+            <div class="relative h-3/5 md:aspect-[3/2] min-h-[400px] md:h-auto">
                 <!-- Map -->
-                <div class="bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] rounded-[24px] p-1 shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out">
-                    <div class="h-3/5 p-2 rounded-t-[12px]">
-                        <img src="public/images/map.png" class="object-cover h-full w-full rounded-[12px]">
-                    </div>
+                <img src="public/images/map.png" class="absolute inset-0 p-2 pb-0 w-full h-full  object-cover rounded-t-2xl">
 
-                    <div class="sm:flex h-2/5">
-                        <!-- Address -->
-                        <div class="p-[10px] sm:w-1/2">
-                            <div class="h-1/3 flex">
-                                <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline">
-                                    1800 Sheppard Ave E u275, North York
-                                </div>
-                            </div>
-
-                            <div class="h-1/3 mt-4 flex">
-                                <div class="dm-sans-bold mt-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline">
+                <!-- Address -->
+                <div class="absolute bottom-2 w-full">
+                    <div class="px-4 h-full">
+                        <div class="flex flex-wrap bg-[#9B5BE9]/70 border-[#7B28E0]/70 border-[1px] backdrop-blur-[5px] w-full px-2 mb-2 rounded-2xl">
+                            <span class="dm-sans-bold m-auto text-[20px] md:text-[24px] text-white font-outline">
+                                1800 Sheppard Ave E u275, North York
+                            </span>
+                        </div>
+                        
+                        <div class="md:flex">
+                            <div class="flex flex-wrap bg-[#9B5BE9]/70 border-[#7B28E0]/70 border-[1px] backdrop-blur-[5px] w-full md:w-[40%] px-2 md:mr-1 rounded-2xl mb-2 md:mb-0">
+                                <span class="dm-sans-bold m-auto text-[20px] md:text-[24px] text-white font-outline">
                                     10am - 9pm
-                                </div>
+                                </span>
                             </div>
 
-                            <div class="h-1/3 flex">
-                                <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline">
+                            <div class="flex flex-wrap bg-[#9B5BE9]/70 border-[#7B28E0]/70 border-[1px] backdrop-blur-[5px] w-full md:w-[60%] px-2 md:ml-1 rounded-2xl">
+                                <span class="dm-sans-bold m-auto text-[20px] md:text-[24px] text-white font-outline">
                                     +1-(416)-492-7181
-                                </div>
+                                </span>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
 
-                        <!-- Conditions -->
-                        <div class="p-[10px] sm:w-1/2 my-auto">
-                            <div class="flex justify-between h-1/4">
-                                <div class="flex flex-wrap bg-blue-600/60 border-blue-500/70 border-[1px] backdrop-blur-[5px] w-full px-2 mb-2 rounded-[12px]">
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline">
-                                        Excellent
-                                    </div>
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline ml-auto">
-                                        ###
-                                    </div>
-                                </div>
-                            </div>
+            <!-- Bottom -->
+            <div class="px-2 my-2">
+                <div class="bg-blue-600/60 border-blue-500 border-[1px] px-2 mb-2 rounded-2xl">
+                    <div class="flex flex-wrap dm-sans-bold text-[18px] md:text-[20px] text-base-900 dark-font-outline w-full h-auto">
+                        Excellent <span class="ml-auto px-2">###</span>
+                    </div>
+                </div>
 
-                            <div class="flex justify-between h-1/4">
-                                <div class="flex flex-wrap bg-green-500/60 border-green-400/70 border-[1px] backdrop-blur-[5px] w-full px-2 mb-2 rounded-[12px]">
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] ttext-base-900 dark-font-outline">
-                                        Great
-                                    </div>
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline ml-auto">
-                                        ###
-                                    </div>
-                                </div>
-                            </div>
+                <div class="bg-green-500/60 border-green-400 border-[1px] px-2 mb-2 rounded-2xl">
+                    <div class="flex flex-wrap dm-sans-bold text-[18px] md:text-[20px] text-base-900 dark-font-outline w-full h-auto">
+                        Very Good <span class="ml-auto px-2">###</span>
+                    </div>
+                </div>
 
-                            <div class="flex justify-between h-1/4">
-                                <div class="flex flex-wrap bg-yellow-500/60 border-yellow-400/70 border-[1px] backdrop-blur-[5px] w-full px-2 mb-2 rounded-[12px]">
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline">
-                                        Good
-                                    </div>
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline ml-auto">
-                                        ###
-                                    </div>
-                                </div>
-                            </div>
+                <div class="bg-yellow-500/60 border-yellow-400 border-[1px] px-2 mb-2 rounded-2xl">
+                    <div class="flex flex-wrap dm-sans-bold text-[18px] md:text-[20px] text-base-900 dark-font-outline w-full h-auto">
+                        Good <span class="ml-auto px-2">###</span>
+                    </div>
+                </div>
 
-                            <div class="flex justify-between h-1/4">
-                                <div class="flex flex-wrap bg-red-600/60 border-red-500/70 border-[1px] backdrop-blur-[5px] w-full px-2 mb-2 rounded-[12px]">
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline">
-                                        Acc.
-                                    </div>
-                                    <div class="dm-sans-bold my-auto text-[32px] lg:text-[24px] text-base-900 dark-font-outline ml-auto">
-                                        ###
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div class="bg-red-600/60 border-red-500 border-[1px] px-2 rounded-2xl">
+                    <div class="flex flex-wrap dm-sans-bold text-[18px] md:text-[20px] text-base-900 dark-font-outline w-full h-auto">
+                        Acceptable <span class="ml-auto px-2">###</span>
                     </div>
                 </div>
             </div>
