a1401986a578cfeaf4267e6bb1ca768d6da78cab
Taha Rashid <taharashid710192@gmail.com>
2025-08-13 20:13:18 -0400

    feat: updated card to use new design

--- components/RetailerCard.vue
+++ components/RetailerCard.vue
@@ -3,61 +3,57 @@
 
 <template>
     <div>
-        <div class="w-[100%] lg:w-[50%]">
-            <div class="flex flex-col lg:mx-5">
+        <div class="w-full">
+            <div class="flex flex-col">
                 <!-- Retailer card -->
-                <div class="sm:flex bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] rounded-[24px] p-1 shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out">
-                    <div class="sm:w-[40%] p-2">
+                <div class="sm:flex bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] rounded-3xl p-1 shadow-[0_0_10px_rgba(38,75,224,0.25)] transition-all duration-300 ease-out">
+                    <div class="sm:w-[50%] px-3 py-1 flex flex-wrap items-center">
                         <!-- Title -->
                         <div class="dm-sans-bold text-[48px] text-base-900 dark-font-outline w-full h-auto">
                             EB Games
                         </div>
 
-                        <!-- Stars -->
-                        <div class="flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-base-900 dark-font-outline ml-1" viewBox="0 0 16 16">
-                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-base-900 dark-font-outline ml-1" viewBox="0 0 16 16">
-                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-base-900 dark-font-outline ml-1" viewBox="0 0 16 16">
-                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-base-900 dark-font-outline ml-1" viewBox="0 0 16 16">
-                                <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z"/>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-base-900 dark-font-outline ml-1" viewBox="0 0 16 16">
-                                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-                            </svg>
-                        </div>
-                    </div>
+                        <div class="flex flex-wrap items-center">
+                            <!-- Stars -->
+                            <div class="flex">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-[#26C1E0] ml-1" viewBox="0 0 16 16">
+                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-[#26C1E0] ml-1" viewBox="0 0 16 16">
+                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-[#26C1E0] ml-1" viewBox="0 0 16 16">
+                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-[#26C1E0] ml-1" viewBox="0 0 16 16">
+                                    <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z"/>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-[#26C1E0] ml-1" viewBox="0 0 16 16">
+                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                                </svg>
+                            </div>
 
-                    <div class="sm:w-[25%] m-2 my-auto">
-                        <div class="bg-green-500/60 border-green-400 border-[1px] p-2 rounded-[12px]">
-                            <div class="dm-sans-bold text-[16px] text-base-900 dark-font-outline w-full h-auto">
-                                $9.99 - $49.99
+                            <!-- Price range -->
+                            <div class="flex pl-2 dm-sans-bold text-[16px]">
+                                <span class="text-[#26C1E0] underline">$9.99</span>
+                                <span class="text-base-900 dark-font-outline">- $49.99</span>
                             </div>
                         </div>
                     </div>
 
-                    <div class="sm:w-[35%] mt-4 sm:mt-0 sm:mt-0 my-auto p-2 gap-y-2">
+                    <div class="sm:w-[50%] my-auto p-1">
                         <!-- Conditons -->
-                        <div class="bg-blue-600/60 border-blue-500 border-[1px] px-2 mb-2 rounded-[12px]">
-                            <div class="dm-sans-bold text-[24px] text-base-900 dark-font-outline w-full h-auto">
-                                New
-                            </div>
-                        </div>
-
-                        <div class="bg-green-500/60 border-green-400 border-[1px] px-2 mb-2 rounded-[12px]">
-                            <div class="dm-sans-bold text-[24px] text-base-900 dark-font-outline w-full h-auto">
-                                Complete
-                            </div>
-                        </div>
-
-                        <div class="bg-yellow-500/60 border-yellow-400 border-[1px] px-2 rounded-[12px]">
-                            <div class="dm-sans-bold text-[24px] text-base-900 dark-font-outline w-full h-auto">
-                                Used
+                        <div class="bg-[#9B5BE9]/70 border-[#7B28E0] border-[1px] px-2 rounded-2xl">
+                            <div class="dm-sans-bold text-[24px] text-white font-outline w-full h-auto">
+                                <div class="flex">
+                                    New <span class="ml-auto">###</span>
+                                </div>
+                                <div class="flex">
+                                    Complete <span class="ml-auto">###</span>
+                                </div>
+                                <div class="flex">
+                                    Used <span class="ml-auto">###</span>
+                                </div>
                             </div>
                         </div>
                     </div>
