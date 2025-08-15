edce9c4f86f1c5c2cad53acfec97cdb6e14ec7ca
Taha Rashid <taharashid710192@gmail.com>
2025-08-15 17:17:25 -0400

    fix: poster fills the container's size

--- components/GamePoster.vue
+++ components/GamePoster.vue
@@ -3,7 +3,7 @@
 
 <template>
     <!-- Outer Card -->
-    <div :id="`game-poster-${props.id}`" class="bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[1px] rounded-2xl p-[5px] shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out will-change-transform">
+    <div :id="`game-poster-${props.id}`" class="flex w-full bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[1px] rounded-xl p-1 shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out">
         <!-- Image -->
         <img :src="props.image" class="w-full aspect-[2/3] object-cover rounded-2xl">
     </div>
