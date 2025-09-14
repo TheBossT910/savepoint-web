f923535c4dceadc097b4248c4d22b04ddd3808c2
Taha Rashid <taharashid710192@gmail.com>
2025-09-13 21:09:48 -0400

    feat: added IGame, IDeveloper, IGenre, ICategory, IImage, IVideo, IGameVariation interfaces

--- types/index.ts
+++ types/index.ts
@@ -18,3 +18,50 @@ export interface IProductCardProp {
     platformLogos: string[]
     generes: string[]
 }
+
+export interface IGame {
+    id: string  // GUID
+    dtc: Date
+    name: string
+    description: string
+    cover: string
+    developers: IDeveloper[]
+    genres: IGenre[]
+    categories: ICategory[]
+    images: IImage[]
+    videos: IVideo[]
+    gameVariations: IGameVariation[]
+}
+
+export interface IDeveloper {
+    id: number
+    name: string
+    country: string
+}
+
+export interface IGenre {
+    id: number
+    name: string
+}
+
+export interface ICategory {
+    id: number
+    name: string
+}
+
+export interface IImage {
+    id: number
+    url: string
+    source: string
+}
+
+export interface IVideo {
+    id: number
+    url: string
+    source: string
+}
+
+export interface IGameVariation {
+    id: number
+    gameIds: string[]   // array of GUIDs
+}
