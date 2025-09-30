1f6c296de4f10d2556ba35d1084239ac01df893c
Taha Rashid <taharashid710192@gmail.com>
2025-09-29 21:05:39 -0400

    chore: updated search index to work with refactored components
    - Renamed images to backgroundImages for clarity

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -3,9 +3,9 @@
         <div class="relative pt-20 pb-10 px-2 sm:px-5 md:px-20 bg-gradient-to-l from-[#26C1E0]/40 to-[#7B28E0]/40">
             <div class="space-y-4 inset-0 h-full w-full object-cover">
                 <!-- Images -->
-                <InfiniteScroll :images="images.slice(0, 3)" :height="'10rem'" :duration="50" class="opacity-0 transition-opacity duration-700 ease-out" :class="loaded ? 'opacity-100' : ''"/>
-                <InfiniteScroll :images="images.slice(3, 6)" :height="'10rem'" reverse :duration="50" class="opacity-0 transition-opacity duration-700 ease-out" :class="loaded ? 'opacity-100' : ''" :style="{ transitionDelay: `200ms` }"/>
-                <InfiniteScroll :images="images.slice(6)" :height="'10rem'" :duration="50" class="opacity-0 transition-opacity duration-700 ease-out" :class="loaded ? 'opacity-100' : ''" :style="{ transitionDelay: `400ms` }"/>
+                <InfiniteScroll :images="backgroundImages.slice(0, 3)" :height="'10rem'" :duration="50" class="opacity-0 transition-opacity duration-700 ease-out" :class="loaded ? 'opacity-100' : ''"/>
+                <InfiniteScroll :images="backgroundImages.slice(3, 6)" :height="'10rem'" reverse :duration="50" class="opacity-0 transition-opacity duration-700 ease-out" :class="loaded ? 'opacity-100' : ''" :style="{ transitionDelay: `200ms` }"/>
+                <InfiniteScroll :images="backgroundImages.slice(6)" :height="'10rem'" :duration="50" class="opacity-0 transition-opacity duration-700 ease-out" :class="loaded ? 'opacity-100' : ''" :style="{ transitionDelay: `400ms` }"/>
 
                 <!-- Overlay on splash image -->
                 <div class="absolute inset-0 bg-black/20"></div>
@@ -21,11 +21,10 @@
 
         <!-- Content -->
         <div class="my-10 mx-2 sm:mx-5 md:mx-20">
-            {{ games }}
             <!-- All games -->
             <div class="grid gap-4 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-                <div v-for="(product, index) in productCardProps" :key="index">  
-                    <LazyGameCard :="product" class="h-full"/>
+                <div v-for="game in games" :key="game.id">  
+                    <LazyGameCard :="game" class="h-full"/>
                 </div>
             </div>
         </div>
@@ -34,35 +33,17 @@
 
 <script setup lang="ts">
 import { getGames } from '~/api/gamesService'
-import type { IGame, IProductCardProp } from '~/types'
+import type { IGame } from '~/types'
 
 const loaded = ref(false)
-const productCardProps = ref<IProductCardProp[]>([])
+const games = ref<IGame[]>();
 
 onMounted(async () => {
     loaded.value = true
-    const games: IGame[] = (await getGames()).data;
-    productCardProps.value = games.map(game => {
-        return {
-            id: 0,              // default
-            coverImage: game.cover,
-            isOwned: false,     // default
-            isFavorite: false,  // default
-            developers: game.developers.map(developer => developer.name),
-            year: 1999,            // default
-            title: game.name,
-            platformLogos: [    // default
-                'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
-                'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
-                'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png'
-            ],
-            generes: game.genres.map(genre => genre.name)
-        }
-    })
+    games.value = (await getGames()).data;
 })
 
-const images = [
+const backgroundImages = [
     'https://shared.steamstatic.com/store_item_assets/steam/apps/1687950/library_600x900_2x.jpg?t=1733297203',
     'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
     'https://shared.steamstatic.com/store_item_assets/steam/apps/2679460/library_600x900_2x.jpg?t=1749196863',
