7983f3dc9585bab41d568bfd4463153fba6edf65
Taha Rashid <taharashid710192@gmail.com>
2025-10-05 17:58:56 -0400

    feat: updated List component to use IGame interface
    - refactored the List component to utilize the IGame interface for props, ensuring type safety and data consistency.
    - updated the component to display game information such as name, developers, genres, and platform logos based on the new IGame interface.
    - also added navigation to product page.

--- components/List.vue
+++ components/List.vue
@@ -3,14 +3,14 @@
 
 <template>
     <!-- Outer Card -->
-    <div :id="`game-card-${props.id}`" class="w-full flex lg:min-h-[170px]">
+    <div :id="`game-card-${props.id}`" class="w-full flex lg:min-h-[170px]" @click="goToProduct">
         <div class="flex w-full h-full bg-gray-100/30 dark:bg-gray-500/30 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[1px] rounded-2xl p-[5px] shadow-[0_0_10px_rgba(38,75,224,0.25)] transition-all duration-300 ease-out will-change-transform">
             <!-- Image -->
             <div class="w-1/3 hidden lg:block">
-                <img :src="props.horizontalImage" class="w-full h-full object-cover rounded-2xl" loading="lazy">
+                <img :src="props.cover" class="w-full h-full object-cover rounded-2xl aspect-16/9" loading="lazy">
             </div>
             <div class="lg:hidden">
-                <img :src="props.horizontalImage" class="absolute inset-0 w-full h-full object-cover rounded-2xl" loading="lazy">
+                <img :src="props.cover" class="absolute inset-0 w-full h-full object-cover rounded-2xl aspect-16/9" loading="lazy">
                 <div class="absolute inset-0 bg-gray-600/20 rounded-2xl backdrop-blur-[2px]"></div>
             </div>
             
@@ -20,11 +20,11 @@
                 <div class="flex flex-wrap items-center mx-4 z-1">
                     <div class="dm-sans-bold text-[32px]">
                         <span class="hidden lg:block text-base-900 dark-font-outline">
-                            {{ props.title }}
+                            {{ props.name }}
                         </span>
 
                         <span class="lg:hidden text-white font-outline">
-                            {{ props.title }}
+                            {{ props.name }}
                         </span>
                     </div>
                 </div>
@@ -45,7 +45,7 @@
                     </div>
 
                     <!-- Reviews -->
-                    <div v-for="review in props.reviews" :key="review.logo" class="flex" >
+                    <div v-for="review in props.reviews" :key="review.id" class="flex" >
                         <img
                         :src="review.logo"
                         class="m-auto lg:h-[17px] md:h-[10px] h-[8px]"
@@ -64,22 +64,24 @@
                     <!-- Developers -->
                     <div class="flex flex-wrap items-center  dm-sans-bold min-h-[16px] text-[12px]">
                         <span class="hidden lg:flex text-gray-400">
-                            {{ props.developers.join(', ') }}, {{ props.year }}
+                            <!-- TODO: add year field -->
+                            {{ props.developers.map(developer => developer.name).join(', ') }}, 20XX
                         </span>
 
                         <span class="flex lg:hidden text-white font-outline">
-                            {{ props.developers.join(', ') }}, {{ props.year }}
+                            <!-- TODO: add year field -->
+                            {{ props.developers.map(developer => developer.name).join(', ') }}, 20XX
                         </span>
                     </div>
 
                     <!-- Genres -->
                     <div class="ml-auto dm-sans-bold text-[12px]">
                         <span class="hidden lg:flex text-gray-400">
-                            {{ props.generes.join(', ') }}
+                            {{ props.genres.map(genre => genre.name).join(', ') }}
                         </span>
 
                         <span class="flex lg:hidden text-white font-outline">
-                            {{ props.generes.join(', ') }}
+                            {{ props.genres.map(genre => genre.name).join(', ') }}
                         </span>
                     </div>
                 </div>
@@ -105,12 +107,12 @@
 
                     <!-- Platform -->
                     <div class="flex mx-2">
-                        <div v-for="logo in props.platformLogos" :key="logo" class="flex mx-1 mb-2 w-full h-full rounded-xl bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] p-[5px] shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out will-change-transform">
+                        <div v-for="platform in props.platforms" :key="platform.id" class="flex mx-1 mb-2 w-full h-full rounded-xl bg-gray-100/30 dark:bg-gray-500/30 hover:bg-gray-300/60 hover:dark:bg-gray-500/60 border-[1px] border-gray-300/70 dark:border-gray-500/70 backdrop-blur-[5px] p-[5px] shadow-sm dark:shadow-gray-400/60 transition-all duration-300 ease-out will-change-transform">
                             <div class="m-auto p-3 hidden lg:block">
-                                <img :src="logo" class="max-h-[25px] m-auto dark:invert" loading="lazy">
+                                <img :src="platform.platformLogo" class="max-h-[25px] m-auto dark:invert" loading="lazy">
                             </div>
                             <div class="m-auto p-3 lg:hidden">
-                                <img :src="logo" class="max-h-[25px] m-auto invert" loading="lazy">
+                                <img :src="platform.platformLogo" class="max-h-[25px] m-auto invert" loading="lazy">
                             </div>
                         </div>
                     </div>
@@ -121,30 +123,11 @@
 </template>
 
 <script setup lang="ts">
-// import type { IProductCardProp } from '~/types';
-// const props = defineProps<IProductCardProp>()
+import type { IGame } from '~/types';
+const router = useRouter();
+const props = defineProps<IGame>()
 
-const props = {
-    id: 0,
-    horizontalImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/header.jpg?t=1744328429',
-    horizontalImage2: 'https://shared.steamstatic.com/store_item_assets/steam/apps/284160/header.jpg?t=1743607204',
-    horizontalImage3: 'https://cdn2.steamgriddb.com/hero_thumb/e295402f4bc2d05c3daba18c489b2dbb.jpg',
-    horizontalImage4: 'https://shared.steamstatic.com/store_item_assets/steam/apps/1687950/header.jpg?t=1733297203',
-    verticalImage: 'https://shared.steamstatic.com/store_item_assets/steam/apps/2161700/library_600x900_2x.jpg?t=1744328429',
-    developers: ['Atlus'],
-    year: 2024,
-    title: 'Persona 3: Reload',
-    generes: ['Action', 'Adventure', 'Horror'],
-    reviews: [
-        {logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/IGN_logo.svg/2560px-IGN_logo.svg.png', rating: '98'},
-        {logo: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Metacritic_M.png', rating: '92'}
-    ],
-    description: 'Dive into the Dark Hour and awaken the depths of your heart. Persona 3 Reload is a captivating reimagining of the genre-defining RPG, reborn for the modern era with cutting-edge graphics and gameplay.',
-    platformLogos: [
-            'https://upload.wikimedia.org/wikipedia/commons/7/7a/PS5_logo.png',
-            'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
-            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Xbox_Logo.svg',
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Steam_2016_logo_black.svg/800px-Steam_2016_logo_black.svg.png',
-        ],
+const goToProduct = () => {
+    router.push(`/products/${props.id}`)
 }
 </script>
